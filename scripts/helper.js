a08b820487c59b1cae8c052967a79937b47c546e
Simeng <simengzeng11@gmail.com>
2019-10-15 11:16:33 -0700

    checkpoint 12-18 completed

---
+++ scripts/helper.js
@@ -0,0 +1,12 @@
+class Helper {
+constructor(song) {
+  this.song = song; 
+}
+  playPauseAndUpdate(song) {
+    const totalTimeElement = $('.total-time');
+    const duration = player.getDuration();
+    totalTimeElement.text( duration );
+    player.playPause(song);
+  }
+}
+var helper = new Helper;
